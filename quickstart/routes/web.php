47dda6ee53bb3bad05473e2749bae5fd99e76b94
Feonixx89 <mr.dmitrykiselev@gmail.com>
2018-03-10 18:35:19 +0400

    доработка

--- quickstart/routes/web.php
+++ quickstart/routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
-Route::get('tasks', 'TasksController@index');
+Route::get('tasks', 'TasksController@index')->name('tasks.index');
+
+Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
+
+Route::post('tasks/store', 'TasksController@store')->name('tasks.store');
