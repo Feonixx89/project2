17ca1255911c463737eb14b2ea7b77e867e088a8
Feonixx89 <mr.dmitrykiselev@gmail.com>
2018-03-10 23:28:41 +0400

    Правка, добавлен метод  PUT

--- quickstart/resources/views/tasks/edit.blade.php
+++ quickstart/resources/views/tasks/edit.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-md-12">
 
-                {!! Form ::open(['route' => ['tasks.update', $task->id]]) !!}
+                {!! Form ::open(['route' => ['tasks.update', $task->id], 'method'=>'PUT']) !!}
 
                 <div class="form-group">
                     <input type="text" class="form-control" name="title" value="{{ $task->title }}">
