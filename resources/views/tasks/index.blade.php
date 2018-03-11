c8106b6fc2ee1eb85b6e87dff2be878944ff51f7
Feonixx89 <mr.dmitrykiselev@gmail.com>
2018-03-11 20:07:43 +0400

    настройка верстки

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,52 +3,48 @@
 @section('content')
 
     <div class="container">
-        <h3>My tasks</h3>
-            <a href="{{ route('tasks.create') }}" class="btn btn-success">Create</a>
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <td>ID</td>
-                                <td>Title</td>
-                                <td>Actions</td>
-                            </tr>
-                            </thead>
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-tasks">
+                    <thead>
+                        <tr>
+                            <td></td>
+                            <td colspan="2"><a href="{{ route('tasks.create') }}" class="btn btn-default btn-none">I want to ...</a></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach( $tasks as $task )
+                        <tr>
+                            <td>
+                                {{ Form::open() }}
+                                <input type="checkbox" name="item" id="item_{{ $task->id }}"
+                                       {{ $task->done ? 'checked' : '' }}
+                                       onclick="this.form.submit()">
+                                <input type="hidden" name="item_id" value="{{ $task->id }}">
+                                {{ Form::close() }}
+                            </td>
+                            <td>{{ $task->id }}</td>
+                            <td>{{ $task->title }}</td>
+                            <td>
+                                <a href="{{ route('tasks.show', $task->id) }}">
+                                    <i class="glyphicon glyphicon-eye-open"></i>
+                                </a>
 
-                            <tbody>
-                            @foreach( $tasks as $task )
-                            <tr>
-                                <td>
-                                    {{ Form::open() }}
-                                        <input type="checkbox" name="item" id="item_{{ $task->id }}" {{ $task->done ? 'checked' : '' }}
-                                               onclick="this.form.submit()">
-                                        <input type="hidden" name="item_id" value="{{ $task->id }}">
-                                    {{ Form::close() }}
-                                </td>
-                                <td>{{ $task->id }}</td>
-                                <td>{{ $task->title }}</td>
-                                <td>
-                                    <a href="{{ route('tasks.show', $task->id) }}">
-                                        <i class="glyphicon glyphicon-eye-open"></i>
-                                    </a>
+                                <a href="{{ route('tasks.edit', $task->id) }}">
+                                    <i class="glyphicon glyphicon-edit"></i>
+                                </a>
 
-                                    <a href="{{ route('tasks.edit', $task->id) }}">
-                                        <i class="glyphicon glyphicon-edit"></i>
-                                    </a>
+                                {{--<a href="#">--}}
+                                    {{--<i class="glyphicon glyphicon-remove"></i>--}}
+                                {{--</a>--}}
 
-                                    <a href="#">
-                                        <i class="glyphicon glyphicon-remove"></i>
-                                    </a>
-
-                                </td>
-                            </tr>
-                            @endforeach
-                            </tbody>
-
-                        </table>
-                    </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 
-    @endsection
+@endsection
